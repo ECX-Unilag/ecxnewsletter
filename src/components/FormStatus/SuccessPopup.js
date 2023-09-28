0fc926c2781c0b9cfb3f4884db0b2c8101bfa121
Nicholas <102188095+nick335@users.noreply.github.com>
2023-09-28 09:53:34 +0100

    modal issues resolved!!

--- src/components/FormStatus/SuccessPopup.js
+++ src/components/FormStatus/SuccessPopup.js
@@ -1,16 +1,22 @@
-import React from 'react' 
+import React, { useEffect } from 'react' 
 import { FaXmark } from 'react-icons/fa6'
 import success from '../../../public/success.webp'
 import Image from 'next/image'
 
 const SuccessPopup = ({display, close}) => {
+
+  useEffect(() => {
+    display ? (document.body.style.overflow = 'hidden') :(document.body.style.overflow = 'auto')
+  }, [display])
+
+
   return (
-    <div className={`${display ? 'flex' : 'hidden'}  absolute top-0 left-0 bg-black/30 w-screen h-screen  items-center justify-center`}>
-     <div className='relative w-[85%] max-w-[400px] py-8 bg-white shadow-lg flex flex-col rounded-md'>
-      <div className='w-fit ml-auto h-fit absolute -right-4 -top-3' onClick={close}>
+    <div className={`${display ? 'flex' : 'hidden'}  fixed top-0 left-0 bg-black/30 w-screen h-screen  items-center justify-center`}>
+     <div className='relative w-[85%] max-w-[400px] pb-8 bg-white shadow-lg flex flex-col rounded-md'>
+      <div className='w-fit ml-auto h-fit mt-1 mr-1' onClick={close}>
        <FaXmark className='text-black text-[2rem]'/>
       </div>
-      <div className='relative w-36 h-36 mx-auto'>
+      <div className='relative w-36 h-36 mx-auto mt-3'>
        <Image alt='succesImage' src={success} className='object-fit' fill={true}  />
       </div>
       <h3 className='mt-6 text-center text-xl font-inter px-2'>Thank you for subscribing to our newsletter</h3>
